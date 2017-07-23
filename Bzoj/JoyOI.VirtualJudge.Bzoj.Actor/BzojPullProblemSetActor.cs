8437d2045da5a94c3a640476c3ee66ce67fb1716
あまみや ゆうこ <1@1234.sh>
2017-07-23 22:54:55 +0800

    Added retry

--- Bzoj/JoyOI.VirtualJudge.Bzoj.Actor/BzojPullProblemSetActor.cs
+++ Bzoj/JoyOI.VirtualJudge.Bzoj.Actor/BzojPullProblemSetActor.cs
@@ -21,42 +21,103 @@ namespace JoyOI.VirtualJudge.Bzoj.Actor
         public void Main()
         {
             client = new HttpClient() { BaseAddress = new Uri(baseUrl) };
+            MainAsync().Wait();
         }
 
         private async Task MainAsync()
         {
-            var problemIds = (await FindProblemAsync(1)).ToList();
-            for (var i = 2; i < await FindMaxPageAsync(); i++)
+            var retryLeftTimes = 3;
+            main:
+            try
             {
-                problemIds.AddRange(await FindProblemAsync(i));
-                await Task.Delay(200);
+                var problemIds = (await FindProblemAsync(1)).ToList();
+                for (var i = 2; i < await FindMaxPageAsync(); i++)
+                {
+                    problemIds.AddRange(await FindProblemAsync(i));
+                    await Task.Delay(200);
+                }
+                File.WriteAllText("problemset.json", JsonConvert.SerializeObject(problemIds));
+                File.WriteAllText("return.json", JsonConvert.SerializeObject(new[] { "problemset.json" }));
             }
-            File.WriteAllText("problemset.json", JsonConvert.SerializeObject(problemIds));
-            File.WriteAllText("return.json", JsonConvert.SerializeObject(new[] { "problemset.json" }));
+            catch (Exception ex)
+            {
+                --retryLeftTimes;
+                if (retryLeftTimes <= 0)
+                {
+                    File.WriteAllText("error.txt", ex.ToString());
+                    File.WriteAllText("return.json", JsonConvert.SerializeObject(new[] { "error.txt" }));
+                }
+                else
+                {
+                    await Task.Delay(3000);
+                    goto main;
+                }
+            }
+            finally
+            {
+                client.Dispose();
+            }
         }
 
         private async Task<int> FindMaxPageAsync()
         {
-            var response = await client.GetAsync(problemsetEndpoint.Replace("{PAGE}", "1"));
-            var html = await response.Content.ReadAsStringAsync();
-            var ret = new List<int>(100);
-            foreach (Match x in PageIndexRegex.Matches(html))
+            var retryLeftTimes = 3;
+            findMaxPage:
+            try
             {
-                ret.Add(int.Parse(x.Value));
+                var response = await client.GetAsync(problemsetEndpoint.Replace("{PAGE}", "1"));
+                var html = await response.Content.ReadAsStringAsync();
+                var ret = new List<int>(100);
+                foreach (Match x in PageIndexRegex.Matches(html))
+                {
+                    ret.Add(int.Parse(x.Value));
+                }
+                return ret.Max();
             }
-            return ret.Max();
+            catch
+            {
+                --retryLeftTimes;
+                if (retryLeftTimes <= 0)
+                {
+                    throw;
+                }
+                else
+                {
+                    await Task.Delay(1000);
+                    goto findMaxPage;
+                }
+            }
         }
 
         private async Task<IEnumerable<int>> FindProblemAsync(int page)
         {
-            var response = await client.GetAsync(problemsetEndpoint.Replace("{PAGE}", page.ToString()));
-            var html = await response.Content.ReadAsStringAsync();
-            var ret = new List<int>(100);
-            foreach (Match x in ProblemIdRegex.Matches(html))
+            var retryLeftTimes = 3;
+            findProblem:
+            try
             {
-                ret.Add(int.Parse(x.Value));
+                var response = await client.GetAsync(problemsetEndpoint.Replace("{PAGE}", page.ToString()));
+                var html = await response.Content.ReadAsStringAsync();
+                var ret = new List<int>(100);
+                foreach (Match x in ProblemIdRegex.Matches(html))
+                {
+                    ret.Add(int.Parse(x.Value));
+                }
+                return ret;
             }
-            return ret;
+            catch
+            {
+
+                --retryLeftTimes;
+                if (retryLeftTimes <= 0)
+                {
+                    throw;
+                }
+                else
+                {
+                    await Task.Delay(1000);
+                    goto findProblem;
+                }
+            }
         }
     }
 }
