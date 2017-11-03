5b9a68f81c9665efbea4a020af800131f63f6c87
Hao Shi <shisoftgenius@gmail.com>
2017-11-03 13:41:53 +0800

    scaffolding leetcode

---
+++ src/JoyOI.VirtualJudge.LeetCode/LeetcodeJudgeActor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoyOI.VirtualJudge.LeetCode
+{
+    public class VirtualJudgeMetadata
+    {
+        public string Source { get; set; }
+        public string ProblemId { get; set; }
+        public string Language { get; set; }
+        public string Code { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class VirtualJudgeResult
+    {
+        public string Result { get; set; }
+        public long TimeUsedInMs { get; set; }
+        public long MemoryUsedInByte { get; set; }
+        public string Hint { get; set; }
+    }
+
+    public class PollResult
+    {
+        public string Result { get; set; }
+        public long TimeUsedInMs { get; set; }
+        public long MemoryUsedInByte { get; set; }
+    }
+
+    class LeetcodeJudgeActor
+    {
+
+    }
+}
