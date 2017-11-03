5c6724d0efe06f35d274af7b0401bcf86c21c2f7
Hao Shi <shisoftgenius@gmail.com>
2017-11-03 18:05:17 +0800

    sending problem code

--- src/JoyOI.VirtualJudge.LeetCode/LeetcodeJudgeActor.cs
+++ src/JoyOI.VirtualJudge.LeetCode/LeetcodeJudgeActor.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System;
 using System.Text;
 
 namespace JoyOI.VirtualJudge.LeetCode
@@ -10,8 +15,12 @@ namespace JoyOI.VirtualJudge.LeetCode
         public string ProblemId { get; set; }
         public string Language { get; set; }
         public string Code { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+    }
+
+    public class VirtualJudgeAccount
+    {
+        public string username { get; set; }
+        public string password { get; set; }
     }
 
     public class VirtualJudgeResult
@@ -31,6 +40,67 @@ namespace JoyOI.VirtualJudge.LeetCode
 
     class LeetcodeJudgeActor
     {
+        private const string baseUrl = "https://leetcode.com";
+        private const string loginEndpoint = "/accounts/login/";
+        private const string problemEndpoint = "/problems/{PROBLEM-NAME}/description/";
+        private const string submitEndpoint = "/problems/{PROBLEM-NAME}/submit/";
+        private const string checkEndpoint = "/submissions/detail/{SUBMISSION-ID}/check/";
 
+        private static Regex csrfTokenRegex = new Regex("(?<=<input type='hidden' name='csrfmiddlewaretoken' value=')[a-zA-Z0-9]{0,}");
+        private static Regex questionId = new Regex("(?<=questionId: \')[0-9]{0,}");
+
+        private static System.Net.CookieContainer container = new System.Net.CookieContainer();
+        private static HttpClient client = new HttpClient(new HttpClientHandler() { CookieContainer = container }) { BaseAddress = new Uri(baseUrl) };
+
+        public static void Main()
+        {
+            var metadata = JsonConvert.DeserializeObject<VirtualJudgeMetadata>(File.ReadAllText("metadata.json"));
+            var account = JsonConvert.DeserializeObject<VirtualJudgeAccount>(File.ReadAllText("account.json"));
+            MainAsync(metadata, account).Wait();
+        }
+
+        private static async Task MainAsync(VirtualJudgeMetadata metadata, VirtualJudgeAccount account) {
+            await GetCredantial(account.username, account.password);
+
+        }
+
+        private static async Task<HttpResponseMessage> GetCredantial(string username, string password)
+        {
+            var logingPageRes = await client.GetAsync(loginEndpoint);
+            var loginPaheHTML = await logingPageRes.Content.ReadAsStringAsync();
+            var csrfToken = csrfTokenRegex.Match(loginPaheHTML).Value;
+            return await client.PostAsync(loginEndpoint, new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                { "csrfmiddlewaretoken", csrfToken },
+                { "login", username },
+                { "password", password },
+                { "remember", "" }
+            }));
+        }
+        private static async Task<string> SubmitCodeAsync(string problemName, string code, string language) {
+            string lang = language.ToLower();
+            switch (language) // leave it as switch to extent in the future
+            {
+                case "C++":
+                    lang = "cpp";
+                    break;
+            }
+            var problemUri = problemEndpoint.Replace("{PROBLEM-NAME}", problemName);
+            var problemRes = await client.GetAsync(problemUri);
+            var problemHTML = await problemRes.Content.ReadAsStringAsync();
+            var problemId = questionId.Match(problemHTML);
+            var submitUri = submitEndpoint.Replace("{PROBLEM-NAME}", problemName);
+            var submitParams = new {
+                data_input = "",
+                judge_type = "large",
+                lang = lang,
+                question_id = problemId,
+                test_mode = false,
+                typed_code = code,
+            };
+            var submitRes = await client.PostAsync(submitUri, new StringContent(
+                JsonConvert.SerializeObject(submitParams),
+                Encoding.UTF8, "application/json"));
+        }
     }
 }
