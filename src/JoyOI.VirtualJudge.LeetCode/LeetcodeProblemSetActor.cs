b1fdeb161df1c2c0df826a9d9851dcab5fc832c3
Hao Shi <shisoftgenius@gmail.com>
2017-11-05 13:12:09 +0800

    ns for leetcode actors

--- src/JoyOI.VirtualJudge.LeetCode/LeetcodeProblemSetActor.cs
+++ src/JoyOI.VirtualJudge.LeetCode/LeetcodeProblemSetActor.cs
@@ -7,7 +7,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JoyOI.VirtualJudge.LeetCode
+namespace JoyOI.VirtualJudge.LeetCode.Actor
 {
     class LeetcodeProblemSetActor
     {
--- src/JoyOI.VirtualJudge.LeetCode/LeetcodePullProblemBodyActor.cs
+++ src/JoyOI.VirtualJudge.LeetCode/LeetcodePullProblemBodyActor.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace JoyOI.VirtualJudge.LeetCode
+namespace JoyOI.VirtualJudge.LeetCode.Actor
 {
     class LeetcodePullProblemBodyActor
     {
