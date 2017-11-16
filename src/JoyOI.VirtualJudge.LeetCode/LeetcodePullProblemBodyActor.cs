fe2c87d47ed965dbdfffa9a8e8b88d3a8626b87b
あまみや ゆうこ <1@1234.sh>
2017-11-16 21:07:34 +0800

    Unlimit Leetcode virtuam memory size

--- src/JoyOI.VirtualJudge.LeetCode/LeetcodePullProblemBodyActor.cs
+++ src/JoyOI.VirtualJudge.LeetCode/LeetcodePullProblemBodyActor.cs
@@ -72,7 +72,7 @@ namespace JoyOI.VirtualJudge.LeetCode.Actor
                   console.log(JSON.stringify(ret)); ");
             File.WriteAllText(jsFile, execJs);
             var p = Process.Start(new ProcessStartInfo("runner") { RedirectStandardInput = true });
-            p.StandardInput.WriteLine("10000 10000");
+            p.StandardInput.WriteLine("10000 10000 -1");
             p.StandardInput.WriteLine(String.Format("node {0}", jsFile));
             p.WaitForExit();
             var templates = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText("stdout.txt"));
