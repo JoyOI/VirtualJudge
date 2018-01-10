bb6c931a02cd057e31230280cd6c81383a1bcd9a
あまみや ゆうこ <1@1234.sh>
2018-01-10 17:39:19 +0800

    Fixes codevs virtual judge

--- src/JoyOI.VirtualJudge.CodeVS/CodeVSJudgeActor.cs
+++ src/JoyOI.VirtualJudge.CodeVS/CodeVSJudgeActor.cs
@@ -57,8 +57,8 @@ namespace JoyOI.VirtualJudge.CodeVS
         public int time_cost { get; set; }
         public string inputname { get; set; }
         public string input { get; set; }
-        public string rightoutput { get; set; }
-        public string useroutput { get; set; }
+        public object rightoutput { get; set; }
+        public object useroutput { get; set; }
         public string results { get; set; }
     }
 
@@ -153,7 +153,7 @@ namespace JoyOI.VirtualJudge.CodeVS
                 case "Pascal":
                     return "pas";
                 default:
-                    throw new NotSupportedException(lang + "has not been supported in this problem source");
+                    throw new NotSupportedException(lang + " has not been supported in this problem source");
             }
         }
 
@@ -218,7 +218,10 @@ namespace JoyOI.VirtualJudge.CodeVS
                 var totalResult = ResultRegex.Match((string)result.status).Value.Trim().Replace("Limit", "").Replace(" ", "");
                 IEnumerable<VirtualJudgeSubStatus> subStatuses = ParseSubStatuses(result.results);
                 if (totalResult == "WrongAnswer")
-                    subStatuses.Single(x => x.SubId == FindHintId(result)).Hint = $"{ result.input } \n\n { result.useroutput } \n\n { result.rightoutput }";
+                    subStatuses.Single(x => x.SubId == FindHintId(result)).Hint =
+                        $"{ result.input } \n\n " +
+                        $"{ (result.useroutput is string ? result.useroutput : ((dynamic)result.useroutput).LastOrDefault()) } \n\n " +
+                        $"{ (result.rightoutput is string ? result.rightoutput : ((dynamic)result.rightoutput).LastOrDefault()) }";
 
                 return new VirtualJudgeResult
                 {
